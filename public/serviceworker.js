24f5208f05f3eaa5b37bc435e0c49ac0c92ab3a3
shreyadhaga <shreyadhaga@gmail.com>
2024-06-04 18:04:44 +0530

    Offline functioning

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,40 +1,53 @@
 const CACHE_NAME = 'version-1';
 const urlsToCache = ['index.html', 'offline.html'];
+const STATIC_ASSETS = [
+    '/index.html',
+    '/apple-touch-icon.png',
+    '/favicon-16x16.png',
+    '/favicon-32x32.png',
+    '/favicon.ico',
+    '/logo192.png',
+    '/logo512.png',
+    '/src/styles.css',
+    '/src/colors.css',
+    '/src/index.js'
+]
+async function preCache() {
+    const cache = await caches.open(CACHE_NAME);
+    return cache.addAll(STATIC_ASSETS)
+}
 
 const self = this;
 // Install SW
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache)
-            })
-    )
+    self.skipWaiting()
+    event.waitUntil(preCache())
 });
 
-
-// listen for requests
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(() => {
-                return fetch(event.request)
-                    .catch(() => caches.match('offline.html'))
-            })
-    )
-});
-
+async function cleanupCache() {
+    const keys = await caches.keys();
+    const keysToDelete = keys.map(key => {
+        if(key !== CACHE_NAME){
+            return caches.delete(key)
+        }
+    })
+    return Promise.all(keysToDelete)
+}
 // Activate the SW
 self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_NAME);
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            Promise.all(cacheNames.map((cacheName) => {
-                if (!cacheWhitelist.includes(cacheName)){
-                    return caches.delete(cacheName)
-                }
-            }))
-        })
-    )
+    event.waitUntil(cleanupCache())
+});
+
+async function fetchAssets(event) {
+    try {
+        const response = await fetch(event.request);
+        return response
+    } catch (err) {
+        const cache = await caches.open(CACHE_NAME);
+        return cache.match(event, request)
+    }
+}
+// listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetchAssets(event))
 });
